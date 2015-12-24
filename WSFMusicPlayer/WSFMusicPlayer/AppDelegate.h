6f89a7b2cf8437eb244a3ffb7daa495e33ae843b
WangShengFeng <loveuqian@qq.com>
2015-12-24 14:52:42 +0800

    first init

---
+++ WSFMusicPlayer/WSFMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSFMusicPlayer
+//
+//  Created by WangShengFeng on 15/12/24.
+//  Copyright © 2015年 WangShengFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
